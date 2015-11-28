a22f5a936b042653fecd12e0244d3dacf0ffb7d9
faustaleonardo <faustaleonardo11@gmail.com>
2015-11-28 20:08:29 +0700

    add additional image resources

---
+++ category_news-body.php
@@ -0,0 +1,303 @@
+		<!-- CATEGORY -->
+		<section class="each-category">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-xs-12">
+						<aside class="category-posts">
+							<div class="category-title">
+								<h2 class="no-margin"><span class="text-uppercase">Category</span>: News</h2>
+							</div>
+							<div class="category-content">
+								<div class="panel no-margin">
+									<div class="panel-body-custom no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/headerRuby-news.jpg" class="img-responsive" alt="hack3"/>
+												</div>
+												<div class="content-caption">
+													<div class="ic">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">500 views</p>
+													</div>
+													<div class="ic">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">3 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>
+										<p class="no-margin author-category">Fausta di <a class="red-link" href="category_news.html">News</a> | <span class="date-category"><small>November 14, 2015</small></span></p>			
+										<a class="black-link" href="#"><h2>Fitur Terbaru Ruby 2.3</h2></a>
+										<p class="each-category-content">Kemarin preview Ruby 2.3 telah dirilis. Pembaharuan ini membawa beberapa tambahan untuk kelas inti Ruby, termasuk beberapa sintaks baru. Berikut beberapa penambahan ...</p>
+										<a class="btn no-padding red-link mrgn" href="#">Read more</a>	
+									</div>
+								</div>
+							</div>
+							<div class="category-content">
+								<div class="panel no-margin">
+									<div class="panel-body-custom no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/report_gfx-news.jpg" class="img-responsive" alt="report_gfx"/>
+												</div>
+												<div class="content-caption">
+													<div class="ic">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">422 views</p>
+													</div>
+													<div class="ic">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">6 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>
+										<p class="no-margin author-category">Roberto di <a class="red-link" href="category_news.html">News</a> | <span class="date-category"><small>November 11, 2015</small></span></p>
+										<a class="black-link" href="#"><h2>Seberapa Berhargakah Teknologi-teknologi Open Source Itu?</h2></a>
+										<p class="each-category-content">The Linux Foundation, merupakan organisasi yang berdedikasi untuk mendukung pengembangan Linux dan collaborative software lainnya. Organisasi ini dibangun pada tahun ...</p>
+										<a class="btn no-padding red-link mrgn" href="#">Read more</a>	
+									</div>
+								</div>
+							</div>
+							<div class="category-content">
+								<div class="panel no-margin">
+									<div class="panel-body-custom no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/springio-news.jpg" class="img-responsive" alt="springio"/>
+												</div>
+												<div class="content-caption">
+													<div class="ic">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">500 views</p>
+													</div>
+													<div class="ic">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">3 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>
+										<p class="no-margin author-category">Sherly di <a class="red-link" href="category_news.html">News</a> | <span class="date-category"><small>November 8, 2015</small></span></p>
+										<a class="black-link" href="#"><h2>Spring Boot 1.3.0 Telah Rilis</h2></a>
+										<p class="each-category-content">Sekarang Spring Boot 1.3.0 telah tersedia diberbagai repositori seperti repo.spring.io, Maven Central dan Bintray. Rilis ini menambah fitur baru yang signifikan dan ...</p>
+										<a class="btn no-padding red-link mrgn" href="#">Read more</a>	
+									</div>
+								</div>
+							</div>							
+							<div class="category-content">
+								<div class="panel no-margin">
+									<div class="panel-body-custom no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/chip-news.jpg" class="img-responsive" alt="chip"/>
+												</div>
+												<div class="content-caption">
+													<div class="ic">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">500 views</p>
+													</div>
+													<div class="ic">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">3 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>
+										<p class="no-margin author-category">Alvin di <a class="red-link" href="category_news.html">News</a> | <span class="date-category"><small>November 7, 2015</small></span></p>		
+										<a class="black-link" href="#"><h2>Raspberry Pi Zero, Komputer Mini Seharga US$5</h2></a>
+										<p class="each-category-content">Selama beberapa tahun terakhir, Raspberry Pi telah menjadi komputer mini yang pas dikantong baik secara fisik maupun harga. Memulai debutnya di tahun 2012, Raspberry ...</p>
+										<a class="btn no-padding red-link mrgn" href="#">Read more</a>	
+									</div>
+								</div>
+							</div>							
+							<div class="category-content">
+								<div class="panel no-margin">
+									<div class="panel-body-custom no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/what-is-drupal-news.jpg" class="img-responsive" alt="what-is-drupal"/>
+												</div>
+												<div class="content-caption">
+													<div class="ic">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">500 views</p>
+													</div>
+													<div class="ic">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">3 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>
+										<p class="no-margin author-category">Fausta di <a class="red-link" href="category_news.html">News</a> | <span class="date-category"><small>November 6, 2015</small></span></p>
+										<a class="black-link" href="#"><h2>Drupal 8.0.0 Telah Rilis</h2></a>
+										<p class="each-category-content">Siapa yang tidak kenal dengan Drupal? Ya, Drupal merupakan salah satu CMS populer yang dibangun dengan bahasa pemrograman PHP. Drupal sangat berguna jika kita ingin ...</p>
+										<a class="btn no-padding red-link mrgn" href="#">Read more</a>	
+									</div>
+								</div>
+							</div>							
+							<div class="category-content">
+								<div class="panel no-margin">
+									<div class="panel-body-custom no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/comodo-news.jpg" class="img-responsive" alt="comodo"/>
+												</div>
+												<div class="content-caption">
+													<div class="ic">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">500 views</p>
+													</div>
+													<div class="ic">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">3 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>
+										<p class="no-margin author-category">Roberto di <a class="red-link" href="category_news.html">News</a> | <span class="date-category"><small>November 5, 2015</small></span></p>
+										<a class="black-link" href="#"><h2>Komodo 9.3 Telah Rilis</h2></a>
+										<p class="each-category-content">Akhirnya pengembang merilis Komodo 9.3 dengan berbagai perubahan. Komodo 9.3 sebenarnya ingin diberi versi Komodo 10, karena ada begitu banyak perubahan dalam rilis ...</p>
+										<a class="btn no-padding red-link mrgn" href="#">Read more</a>	
+									</div>
+								</div>
+							</div>
+							<!-- PAGINATION -->
+							<nav class="nav-pagination">
+								<div class="text-center">
+									<ul class="pagination">
+										<li>
+										<a href="#" aria-label="Previous">
+											<span aria-hidden="true">&laquo;</span>
+										</a>
+										</li>
+										<li class="on"><a href="#">1</a></li>
+										<li><a href="#">2</a></li>
+										<li><a href="#">3</a></li>
+										<li><a href="#">4</a></li>
+										<li><a href="#">5</a></li>
+										<li>
+										<a href="#" aria-label="Next">
+											<span aria-hidden="true">&raquo;</span>
+										</a>
+										</li>
+									</ul>
+								</div>
+							</nav>	
+							<!-- END: PAGINATION -->
+						</aside>
+					</div>
+					<div class="col-sm-4 col-xs-12">
+						<aside>
+							<div class="recommended-title">
+								<h3 class="text-uppercase no-margin">Artikel Pilihan</h3>
+							</div>
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/fowab-event.jpg" class="img-responsive" alt="fowab"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>Bandung Developer Day, Harinya Para Developer Kota Bandung</h4></a>
+									</div>
+								</div>	
+							</div>						
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/header-codepolitan-logo-python-tools.jpg" class="img-responsive" alt="header-codepolitan-logo-python-tools"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>Empat IDE Gratis Untuk Python</h4></a>
+									</div>
+								</div>	
+							</div>						
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/cover_header-skill-programming-tipsdantrik.jpg" class="img-responsive" alt="cover_header-skill-programming-tipsdantrik"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>Fitur Umum Sebuah CRUD yang Wajib Dikuasai Pemula</h4></a>
+									</div>
+								</div>	
+							</div>						
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/robovm-tools.jpg" class="img-responsive" alt="robovm"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>RoboVM Kini Tak Lagi Open Source</h4></a>
+									</div>
+								</div>	
+							</div>						
+						</aside>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END: CATEGORY -->
---
+++ category_news.php
@@ -0,0 +1,6 @@
+<?php
+	$page = "category_news";
+	include "header.php";
+	include  "category_news-body.php";
+	include "footer.php";
+?>
