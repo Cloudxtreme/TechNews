74e477f8d69b9143427559c6e9330e691a5b6383
faustaleonardo <faustaleonardo11@gmail.com>
2015-11-30 23:02:11 +0700

    finalize full page post

---
+++ punya-skill-python-yuk-kerja-remote-body.php
@@ -0,0 +1,290 @@
+		<!-- EACH CERTAIN POSTS -->
+		<section class="each-certain-posts">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 col-xs-12">
+						<!-- CERTAIN POSTS -->
+						<aside class="certain-posts">
+							<img class="img-responsive img-certain-posts" src="assets/img/kerja_remote-info.jpg" alt="kerja_remote">
+							<h1 class="title-certain-posts">Punya Skill Python? Yuk Kerja Remote</h1>
+							<div class="authorandshare">
+								<div class="row">
+									<div class="col-xs-12 col-sm-11">
+										<div class="row">
+											<div class="col-xs-12 col-sm-9">
+												<p class="no-margin author-certain-posts">Roberto di <a class="red-link" href="category_news.html">Info</a> | <span class="date-category"><small>November 18, 2015</small></span></p>
+											</div>
+											<div class="col-xs-12 col-sm-3">
+												<div class="dropdown share-dropdown">
+													<button class="btn btn-default dropdown-toggle" type="button" id="share-dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+													<img class="icon-share" src="assets/img/icon/social-share.png"/>
+													<span class="text-uppercase">Share</span>
+													<span class="caret"></span>
+													</button>
+													<ul class="dropdown-menu" aria-labelledby="share-dropdownMenu">
+														<li><a href="#"><img class="icon-share" src="assets/img/icon/facebook-share.png"/>Facebook</a></li>
+														<li><a href="#"><img class="icon-share" src="assets/img/icon/twitter-share.png"/>Twitter</a></li>
+														<li><a href="#"><img class="icon-share" src="assets/img/icon/googleplus-share.png"/>Google Plus</a></li>
+														<li><a href="#"><img class="icon-share" src="assets/img/icon/linkedin-share.png"/>Linkedin</a></li>
+													</ul>
+												</div>
+											</div>
+											<div class="clearfix"></div>
+											<div class="col-xs-12">
+												<div class="content-certain-posts">
+													<p class="pa-content">Python merupakan bahasa pemrograman yang mudah dibaca dan dapat digunakan untuk berbagai macam proyek. Apa yang harus dilakukan setelah menguasai Python? Hal yang bisa kita lakukan salah satunya menambah pendapatan dengan kerja di Software House. Namun bila terhalang masalah geografis, karena jauh dari kota besar, kita bisa mencoba kerja remote.</p>
+													<p class="pa-content">Tren kerja remote mulai menggeliat di Indonesia, namun untuk proyek Python masih jarang. Jika pembaca ingin mencoba kerja remote dengan orang luar negeri, kita bisa mencari peluangnya di situs-situs berikut:</p>
+													<h2>Stack Overflow Careers</h2>
+													<img class="img-responsive" src="assets/img/stackoverflow.png"/>
+													<p class="pa-content">Keuntungan yang kita dapat dalam situs ini antara lain: 1) Jumlah proyek untuk Python cukup banyak, 2) Kualitas para pemberi kerja dengan proyek yang cukup rinci.</p>
+													<p class="pa-content">Kunjungi situs resminya di <a class="red-link" href="#">http://careers.stackoverflow.com</a></p>
+													<h2>FlexJobs</h2>
+													<img class="img-responsive" src="assets/img/flexjobs.jpg"/>
+													<p class="pa-content">Situs ini menawarkan berbagai proyek Python untuk semua posisi. Pasarnya juga bagus dan menggabungkan antara perusahaan dan startup.</p>
+													<p class="pa-content">Kunjungi situs resminya di <a class="red-link" href="#">http://www.flexjobs.com</a></p>
+													<h2>DjangoGigs</h2>
+													<img class="img-responsive" src="assets/img/gigs.png"/>
+													<p class="pa-content">Situs ini juga memberikan peluang proyek Python yang cukup besar. Apabila pembaca menguasai framework Django, ini menjadi poin yang bagus, karena DjangoGigs memang fokus untuk menyediakan pekerjaan bagi pengguna framework Django.</p>
+													<p class="pa-content">Kunjungi situs resminya di <a class="red-link" href="#">http://www.djangogigs.com</a></p>
+													<h2>Skip The Drive</h2>
+													<img class="img-responsive" src="assets/img/skipthedrive.png"/>
+													<p class="pa-content">Jika menggunakan situs ini kita tak perlu melakukan penyaringan proyek. Sedangkan untuk posisi yang disediakan kebanyakan perusahaan maupun agensi.</p>
+													<p class="pa-content">Kunjungi situs resminya di <a class="red-link" href="#">http://www.skipthedrive.com</a></p>
+													<h2>Simply Hired</h2>
+													<img class="img-responsive" src="assets/img/simplyhired.png"/>
+													<p class="pa-content">Simply Hired menyediakan berbagai proyek populer, untuk mencarinya gunakan top ranking. Banyak pilihan yang bisa digunakan untuk penyaringan lokasi maupun kata kunci proyek, diantaranya Remote, Telecommute atau Anywhere. Simply Hired juga menggabungkan perusahaan dan startup untuk posisi pekerjaan yang diinginkan.</p>
+													<p class="pa-content">Kunjungi situs resminya di <a class="red-link" href="#">http://www.simplyhired.com</a></p>
+													<h2>Linkedin</h2>
+													<img class="img-responsive" src="assets/img/linkedin-post.png"/>
+													<p class="pa-content">Situs yang paling dikenal bagi banyak orang. Untuk proyek Python, Linkedin juga menyediakan banyak peluang. Namun biasanya untuk perusahaan saja, dan opsi yang bisa gunakan untuk penyaringan adalah Telecommute.</p>
+													<p class="pa-content">Kunjungi situs resminya di <a class="red-link" href="#">http://www.linkedin.com</a></p>
+													<p class="pa-content">Proyek Python memang masih jarang di Indonesia, namun bukan berarti Python lemah. Sebenarnya proyek Python lebih banyak dicari di level internasional. Buktinya situs-situs tersebut masih menyediakan proyek Python yang melimpah. Nah jika pembaca seorang programmer Python, jangan khawatir peluang akan selalu ada. Selamat bekerja remote.</p>
+												</div>	
+											</div>										
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="clearfix"></div>
+							<!-- RELATED POSTS -->
+							<div class="related-posts">
+								<h3 class="no-margin">Artikel Terkait</h3>
+								<div class="row">
+									<div class="col-xs-12 col-sm-4">
+										<div class="panel">
+											<div class="panel-body no-padding">
+												<a href="#">
+													<div class="content-item">
+														<div class="thumb">
+															<img src="assets/img/header-codepolitan-logo-python-tools.jpg"class="img-responsive" alt="header-codepolitan-logo-python"/>
+														</div>
+													</div>
+												</a>
+												<a class="black-link" href="#"><h4>Empat IDE Gratis untuk Python</h4></a>					
+											</div>
+										</div>
+									</div>									
+									<div class="col-xs-12 col-sm-4">
+										<div class="panel">
+											<div class="panel-body no-padding">
+												<a href="#">
+													<div class="content-item">
+														<div class="thumb">
+															<img src="assets/img/buku-info.jpg"class="img-responsive" alt="buku"/>
+														</div>
+													</div>
+												</a>
+												<a class="black-link" href="#"><h4>Buku Ilmu Komputer Gratis dari Green Tea Press</h4></a>
+											</div>
+										</div>
+									</div>
+									<div class="col-xs-12 col-sm-4">
+										<div class="panel">
+											<div class="panel-body no-padding">
+												<a href="#">
+													<div class="content-item">
+														<div class="thumb">
+															<img src="assets/img/mobile-info.jpg"class="img-responsive" alt="mobile"/>
+														</div>
+													</div>
+												</a>
+												<a class="black-link" href="#"><h4>Seberapa Banyak Sih Developer Mobile yang Jadi Miliarder?</h4></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<!-- END: RELATED POSTS -->
+							<div class="comments-header">
+								<div class="total-comments"><h4>3 Comments</h4></div>
+								<div class="pull-right login-to-comment"><a class="red-link" href="#"><h4>Log in</h4></a></div>
+							</div>
+							<div class="comments-share">
+								<a class="red-link" href="#">
+									<span class="glyphicon glyphicon-share" aria-hidden="true"></span> Share
+								</a>
+							</div>
+							<div class="comments-content">
+								<div class="each-comments-content">
+									<div class="each-comments-person">
+										<img src="assets/img/person.png" class="img-circle img-comment" alt="person"/>
+									</div>
+									<form class="each-comments-form">
+										<textarea class="form-control" id="comment" rows="2" placeholder="Join the discussion..."></textarea>
+									</form>
+								</div>
+							</div>
+							<div class="comments-content">
+								<div class="each-comments-content">
+									<div class="each-comments-person">
+										<img src="assets/img/jansen.png" class="img-circle img-comment" alt="jansen"/>
+									</div>
+									<div class="each-spesific-comments">
+										<p class="no-margin">Jansen - <small class="time-comment">3 days ago</small></p>
+										<p class="no-margin">Hmmm. Diriku masih berkutat pada PHP.</p>
+										<a class="red-link" href="#">
+											<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Reply
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="comments-content">
+								<div class="each-comments-content">
+									<div class="each-comments-person">
+										<img src="assets/img/cy.png" class="img-circle img-comment" alt="cy"/>
+									</div>
+									<div class="each-spesific-comments">
+										<p class="no-margin">Christian - <small class="time-comment">2 days ago</small></p>
+										<p class="no-margin">Baru-baru ini saya belajar python. Pas banget artikelnya.</p>
+										<a class="red-link" href="#">
+											<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Reply
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="comments-content-reply">
+								<div class="each-comments-content">
+									<div class="each-comments-person">
+										<img src="assets/img/jansen.png" class="img-circle img-comment" alt="jansen"/>
+									</div>
+									<div class="each-spesific-comments">
+										<p class="no-margin">Jansen - <small class="time-comment">1 days ago</small></p>
+										<p class="no-margin">Hebat ! Ajari saya dong. Hehe.</p>
+										<a class="red-link" href="#">
+											<span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Reply
+										</a>
+									</div>
+								</div>
+							</div>
+						</aside>
+						<!-- END: CERTAIN POSTS -->
+					</div>
+					<div class="col-sm-4 col-xs-12">
+						<!-- RECOMMENDED POSTS -->
+						<aside class="recommended-posts">
+							<div class="recommended-title">
+								<h3 class="text-uppercase no-margin">Artikel Pilihan</h3>
+							</div>
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/fowab-event.jpg" class="img-responsive" alt="fowab"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>Bandung Developer Day, Harinya Para Developer Kota Bandung</h4></a>
+									</div>
+								</div>	
+							</div>						
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/header-codepolitan-logo-python-tools.jpg" class="img-responsive" alt="header-codepolitan-logo-python-tools"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>Empat IDE Gratis Untuk Python</h4></a>
+									</div>
+								</div>	
+							</div>						
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/cover_header-skill-programming-tipsdantrik.jpg" class="img-responsive" alt="cover_header-skill-programming-tipsdantrik"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>Fitur Umum Sebuah CRUD yang Wajib Dikuasai Pemula</h4></a>
+									</div>
+								</div>	
+							</div>						
+							<div class="recommended-content">
+								<div class="panel">
+									<div class="panel-body no-padding">
+										<a href="#">
+											<div class="content-item">
+												<div class="thumb">
+													<img src="assets/img/robovm-tools.jpg" class="img-responsive" alt="robovm"/>
+												</div>
+												<div class="content-caption">
+													<div class="icp">
+														<img class="img-ic1" src="assets/img/icon/view.png" href="#" alt="view"/>
+														<p class="no-margin text-right number-vc">600 views</p>
+													</div>
+													<div class="icp">
+														<img class="img-ic2" src="assets/img/icon/comment.png" href="#" alt="comment"/>
+														<p class="no-margin text-left number-vc">10 comments</p>
+													</div>	
+												</div>
+											</div>
+										</a>										
+										<a class="black-link" href="#"><h4>RoboVM Kini Tak Lagi Open Source</h4></a>
+									</div>
+								</div>	
+							</div>						
+						</aside>
+						<!-- END: RECOMMENDED POSTS -->
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- END: CATEGORY -->		
---
+++ punya-skill-python-yuk-kerja-remote.php
@@ -0,0 +1,5 @@
+<?php
+	include "header.php";
+	include  "punya-skill-python-yuk-kerja-remote-body.php";
+	include "footer.php";
+?>
