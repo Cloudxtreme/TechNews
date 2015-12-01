d956a00c1c3a69fef33904d570d2c52ac87e1aa8
faustaleonardo <faustaleonardo11@gmail.com>
2015-12-01 15:51:11 +0700

    comment out preloading for page

--- js/script.js
+++ js/script.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
 	     color           : "rgb(255, 255, 255)",
 	     image           : "assets/img/loading.gif",
 	});	
-/*	if (!$('footer').hasClass('navbar-fixed-bottom')){
+	if (!$('footer').hasClass('navbar-fixed-bottom')){
 		$.LoadingOverlay("show");
 		setTimeout(function(){
 		    $.LoadingOverlay("hide");
 		}, 1000);
-	}*/
-
+	}
+	
 	$('#sendBtn').click(function(){
 		swal("Thank you", "Your message was sent successfully")
 	})
