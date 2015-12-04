01811bd9200ef3e5369459885c7ff51a3984552a
faustaleonardo <faustaleonardo11@gmail.com>
2015-12-04 13:47:14 +0700

    add account.php

--- login-body.php
+++ login-body.php
@@ -3,15 +3,15 @@
 			<div class="container">
 				<div class="panel panel-custom">
 					<div class="panel-body">
-						<h2 class="text-center text-ls">Log in to continue</h2>
-						<form class="form-ls">
-						  	<div class="form-group form-ls-input">
+						<h2 class="text-center text-lsa">Log in to continue</h2>
+						<form class="form-lsa">
+						  	<div class="form-group form-lsa-input">
 							    <input type="text" class="form-control" id="emailorusername" placeholder="Email or username"/>
 						  	</div>
-						  	<div class="form-group form-ls-input">
+						  	<div class="form-group form-lsa-input">
 							    <input type="password" class="form-control" id="password" placeholder="Password"/>
 						  	</div>
-						  	<button type="button" class="btn text-uppercase btn-ls" id="loginbtn"><?php echo ($role == 'myadmin' ? 'Log in as admin' : 'Log in') ?></button>
+						  	<button type="button" class="btn text-uppercase btn-lsa" id="loginbtn"><?php echo ($role == 'myadmin' ? 'Log in as admin' : 'Log in') ?></button>
 						  	<a class="text-center fyp red-link" href="#">Forgot your password?</a>
 						</form>
 					</div>
--- signup-body.php
+++ signup-body.php
@@ -2,18 +2,18 @@
 			<div class="container">
 				<div class="panel panel-custom">
 					<div class="panel-body">
-						<h2 class="text-center text-ls">Sign up to join</h2>
-						<form class="form-ls">
-						  	<div class="form-group form-ls-input">
+						<h2 class="text-center text-lsa">Sign up to join</h2>
+						<form class="form-lsa">
+						  	<div class="form-group form-lsa-input">
 							    <input type="text" class="form-control" id="username" placeholder="Username"/>
 						  	</div>
-						  	<div class="form-group form-ls-input">
+						  	<div class="form-group form-lsa-input">
 							    <input type="email" class="form-control" id="email" placeholder="Email"/>
 						  	</div>
-						  	<div class="form-group form-ls-input">
+						  	<div class="form-group form-lsa-input">
 							    <input type="password" class="form-control" id="password" placeholder="Password"/>
 						  	</div>
-						  	<button type="button" class="btn text-uppercase btn-ls" id="signupbtn">Sign up</button>
+						  	<button type="button" class="btn text-uppercase btn-lsa" id="signupbtn">Sign up</button>
 						</form>
 					</div>
 			  	</div>
